39d6ecbbe89a258562b43ebfb931604c6630ad01
goodlearner10 <hee001116@gmail.com>
2023-02-14 22:59:20 +0900

    feat: 모델링 구현

--- README.md
+++ README.md
@@ -27,12 +27,100 @@
 ## 3차 요구사항
 
 타 수강생의 타이핑 즉 설계 & 모델링을 실제로 구현합니다.
-[구현한 레파지토리 링크](https://github.com/seul-dev/wanted-pre-onboarding-challenge-fe-2)
 
-- [] 설계 & 모델링된 TypeScript's Interface를 따라 내부 구현부를 모두 작성한다.
+> **seul**님이 설계한 코드를 구현했습니다  
+> [레파지토리 링크](https://github.com/seul-dev/wanted-pre-onboarding-challenge-fe-2)
+
+- [x] 설계 & 모델링된 TypeScript's Interface를 따라 내부 구현부를 모두 작성한다.
   - 동작되지 않더라도 설계 & 모델링을 지키는 것이 원칙이다.
-- [] 절대로 동작과 구현을 위해 설계 & 모델링 규칙을 어기지 않습니다.
-- [] README.md 혹은 별도의 문서에 어떤 부분의 타입을 고치면 좋은지 피드백과 출처 링크를 남깁니다.
+- [x] 절대로 동작과 구현을 위해 설계 & 모델링 규칙을 어기지 않습니다.
+- [x] README.md 혹은 별도의 문서에 어떤 부분의 타입을 고치면 좋은지 피드백과 출처 링크를 남깁니다.
+
+## 구현 후기
+
+### TodoService 클래스
+
+```ts
+class TodoService {
+  private todoList: TodoItem[];
+  private todoId: number;
+  private tagId: number;
+
+  constructor() {
+    this.todoList = [];
+    this.todoId = Date.now();
+    this.tagId = Date.now();
+  }
+}
+```
+
+- 각각의 투두와 태그들을 분별하기 위한 id와 투두 객체를 담는 배열을 동시에 생성합니다.
+
+- 위 `TodoService` 클래스는 인스턴스 생성시에만 아이디를 생성할 수 있기 때문에 `createTodo()` 메서드를 사용하여 투두 객체를 추가하면 `todoList` 배열에는 모두 같은 아이디 값을 가진 투두들이 저장됩니다. (태그 아이디도 마찬가지 입니다.)
+
+  - 이렇게 모두 같은 값이 아이디에 부여되므로 아이디는 분별 역할을 수행할 수 없습니다.
+
+- 새 투두나 태그를 생성할 때 `Date.now()`를 사용해 아이디를 생성하는 방식이 더 좋을 것 같습니다.
+
+✔️ 위와 같은 이유로 id를 사용하여 할 일을 지우거나 수정하는 일들이 불가능하지만, 객체마다 다른 아이디를 가지고 있음을 가정하고 설계에 맞춰 코드를 구현하였습니다.
+
+✔️ 작성한 JSdoc 문서와 인터페이스 모델링 간의 불일치가 있었습니다. interface 모델링을 우선 반영하여 작성했습니다.
+
+### createTodo 메서드
+
+```js
+
+  /**
+   * Create a new Todo item.
+   * @param {string} [content=''] - The content of the Todo item.
+   * @throws {Error} If the content is empty.
+   * @returns {TodoItem} The created Todo item.
+   */
+  createTodo(content) {}
+
+```
+
+- content 인자를 받은 뒤 투두 객체를 만들어 배열에 추가한 뒤 객체를 리턴합니다.
+- content 인자는 기본값으로 `''` 빈 문자열을 가집니다.
+- content 값이 비어있다면 에러를 발생시킵니다
+
+**나의 생각**
+
+- 내용 없이도 할 일을 추가할 수 있어야 한다는 요구와 맞지 않습니다.
+- content 값이 비어있어도 에러를 발생시키지 않고 객체를 생성할 수 있어야 한다고 생각합니다.
+- 기본값은 그대로 두고 error 구문은 삭제하면 내용 없이도 할 일을 추가할 수 있게 됩니다.
+
+### Error
+
+```js
+  * @throws {Error} If the todo item with the given id is not found.
+```
+
+- 주어진 아이디에 해당하는 투두를 찾지 못할 시 에러를 발생시킵니다.
+- 설계에서 위 내용이 메서드마다 반복해서 등장합니다.
+
+**나의 생각**
+
+- 동일한 코드가 반복되게 되므로 해당 부분은 따로 메서드로 빼서 사용하는 것이 좋을 것 같습니다.
+- 모델링 규칙을 어기지 않는 것이 조건이기 때문에 새로운 메서드를 만들지 않고 설계대로 코드를 반복하여 작성하였습니다.
+
+### optional과 Default Parameters
+
+```js
+  * @param {string} [content=''] - The content of the Todo item
+  * @param {string} [category=''] - The new category of the todo item.
+```
+
+```ts
+createTodo(content: string): void;
+updateTodoCategoryById(todoId: number, category?: string): void;
+```
+
+- 기본값을 가지는 인자에 대한 타입 정의가 다릅니다.
+
+**나의 생각**
+
+- 일치가 필요해 보입니다.
 
 ### Todo
 
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ interface TodoItem {
   tags?: TagItem[];
 }
 
-interface TodoService {
+interface ITodoService {
   createTodo(content: string): void;
   createTodoTagById(todoId: number, ...tags: string[]): void;
   readTodoById(todoId: number): TodoItem;
@@ -23,4 +23,141 @@ interface TodoService {
   deleteTodoById(todoId: number): void;
   deleteAllTodos(): void;
   deleteAllTagsById(todoId: number): void;
+}
+
+class TodoService implements ITodoService {
+  private todoList: TodoItem[];
+  private todoId: number;
+  private tagId: number;
+
+  constructor() {
+    this.todoList = [];
+    this.todoId = Date.now();
+    this.tagId = Date.now();
+  }
+
+  createTodo(content: string = '') {
+    if (content === '') {
+      throw new Error('content 값이 빈 문자열입니다.');
+    }
+
+    this.todoList.push({
+      id: this.todoId,
+      content: content,
+      isDone: false,
+      category: '',
+      tags: [],
+    });
+  }
+
+  createTodoTagById(todoId: number, ...tags: string[]) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    const tagArr: TagItem[] = [];
+
+    // id넣은 객체 생성
+    tags.map(tag => {
+      let obj = {
+        id: this.tagId,
+        content: tag,
+      };
+      tagArr.push(obj);
+    });
+
+    todo.tags = todo.tags?.concat(tagArr);
+  }
+
+  readTodoById(todoId: number) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    return todo;
+  }
+
+  readTodos() {
+    return this.todoList;
+  }
+
+  updateTodoContentById(todoId: number, content: string) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    todo.content = content;
+  }
+
+  updateTodoIsDoneById(todoId: number) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    todo.isDone = !todo.isDone;
+  }
+
+  updateTodoCategoryById(todoId: number, category = '') {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    if (category === '') {
+      throw new Error('카테고리 내용을 입력해주세요.');
+    }
+
+    todo.category = category;
+  }
+
+  updateTodoTagById(todoId: number, tagId: number, tagContent: string) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    //tag id error
+    const tagObj = todo.tags?.find(tag => tag.id === tagId);
+
+    if (!tagObj) {
+      throw new Error('태그를 찾을 수 없습니다.');
+    }
+
+    tagObj.content = tagContent;
+  }
+
+  deleteTodoById(todoId: number) {
+    const index = this.todoList.findIndex(todo => todo.id === todoId);
+
+    if (!this.todoList[index]) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    this.todoList.splice(index, 1);
+  }
+
+  deleteAllTodos() {
+    this.todoList = [];
+    return this.todoList;
+  }
+
+  deleteAllTagsById(todoId: number) {
+    const todo = this.todoList.find(todo => todo.id === todoId);
+
+    if (!todo) {
+      throw new Error('해당하는 할 일을 찾을 수 없습니다.');
+    }
+
+    todo.tags = [];
+  }
 }
